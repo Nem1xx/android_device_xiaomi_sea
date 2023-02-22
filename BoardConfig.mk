11275eefc7a7252b91a9116925c64c2e694efacd
Alistair Delva <adelva@google.com>
2023-02-22 14:43:10 -0800

    sea: Set androidboot.serialconsole= for serial console
    Don't remove the old way so we can still use older kernels.
    
    Bug: 266982931
    Bug: 223797063
    Bug: 267428635
    Change-Id: I8909662220014207af0783d403147a0d0edb3b10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,8 @@ BOARD_RAMDISK_USE_LZ4 := true
 BOARD_KERNEL_CMDLINE := \
     bootopt=64S3,32N2,64N2 \
     cgroup.memory=nokmem \
-    sysctl.kernel.sched_pelt_multiplier=4
+    sysctl.kernel.sched_pelt_multiplier=4 \
+    androidboot.serialconsole=0
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sea
