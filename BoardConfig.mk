e50bd79158aa1079678e6499c1a5717759f3a39b
guibing <guibing@google.com>
2023-10-25 18:03:49 +0000

    sea: Disable kmem cgroup accounting
    Disable cgroup kernel memory accounting to avoid unnecessary overhead
    
    This is from similar change in zuma: ag/22891292
    
    Bug: 300516320
    Test: Boot
    Change-Id: I1378fe11db294449978ea8e0157e8d8964a9a112

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,7 +64,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 
 BOARD_KERNEL_CMDLINE := \
-    bootopt=64S3,32N2,64N2
+    bootopt=64S3,32N2,64N2 \
+    cgroup.memory=nokmem
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sea
