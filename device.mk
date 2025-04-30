3546943e4bb466e780c6924d37021a8fd4769646
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-04-30 02:04:23 +0200

    sea: Enable CameraX extensions support
    - Set `ro.camerax.extensions.enabled=true` in product.prop
    - Add `camerax-vendor-extensions.jar` to system
    - Grant required permission via `camerax-vendor-extensions.xml`
    
    Enables support for enhanced camera features via CameraX Extensions, improving compatibility with modern camera apps.
    
    Change-Id: I491c39389793908e11db4172b2a67bccdaf0aa8e

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera Extensions
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/camerax-vendor-extensions.xml
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
